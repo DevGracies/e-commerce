d88db6915e62cb0798094083d268c05559fd4550
Grace <graceadegunle16@gmail.com>
2023-08-26 19:34:59 +0100

    commit

--- src/component/Register/Login.jsx
+++ src/component/Register/Login.jsx
@@ -7,36 +7,59 @@ const Login = () => {
 
   const handelChange = (e) => {
     const { name, value } = e.target;
+    if (name === "Email") {
+      setEmail(value);
+    }
+    if (name === "Password") {
+      setPassword(value);
+    }
+    console.log(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const obj = {
+      email: email,
+      password: password,
+    };
+    console.log(obj);
   };
   return (
-    <div className="xl:flex">
-      <div className="">
+    <div className="w-70 m-10 xl:flex xl:justify-between">
+      <div className=" sm:hidden xl:inline" alt="login page">
         <img src={login} alt="" />
       </div>
-      <div className="">
-        <h1>Log in to Exclusive</h1>
-        <h6>Enter your details below</h6>
+      <form
+        className="justify-center grid self-center items-center"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-4xl text-center mt-3 mb-3">Log in to Exclusive</h1>
+        <h6 className=" text-xl text-center">Enter your details below</h6>
         <input
+          className="border-b-2 w-72 outline-none border-black m-10"
           type="text"
           value={email}
           name="Email"
-          id="Email"
           onChange={handelChange}
           placeholder="Email or Phone Number"
         />
         <input
+          className="border-b-2 w-72 outline-none border-black m-10"
           type="text"
           value={password}
           name="Password"
-          id="Password"
           onChange={handelChange}
           placeholder="Password"
         />
-        <div className="">
-          <button>Login</button>
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className=" bg-[#DB4444] text-white font-medium w-40 p-2 rounded"
+          >
+            Login
+          </button>
           <button>Forget Password</button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
