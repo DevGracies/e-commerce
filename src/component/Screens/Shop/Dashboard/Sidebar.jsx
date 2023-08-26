9d9e540ab4fe1b7961c38f59d1ced956e870803c
Grace <graceadegunle16@gmail.com>
2023-08-26 20:24:51 +0100

    commit

--- src/component/Screens/Shop/Dashboard/Sidebar.jsx
+++ src/component/Screens/Shop/Dashboard/Sidebar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { FaLeaf } from "react-icons/fa";
+import { BsFillBagCheckFill, BsLock } from "react-icons/bs";
+import { HiUsers } from "react-icons/hi";
+import {
+  AiFillAccountBook,
+  AiOutlineUser,
+  AiOutlineSetting,
+  AiOutlineUserAdd,
+} from "react-icons/ai";
+import { VscError } from "react-icons/vsc";
+import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
 
+import { BiDollar } from "react-icons/bi";
+import { AiOutlineSync } from "react-icons/ai";
+import { TfiMenuAlt } from "react-icons/tfi";
+
+import { HiComputerDesktop, HiDeviceTablet } from "react-icons/hi";
+import { BsTelephoneFill } from "react-icons/bs";
+import { IoMdNotifications } from "react-icons/io";
+import {
+  AiOutlineDollarCircle,
+  AiOutlineSearch,
+  AiOutlineArrowUp,
+  AiOutlineArrowDown,
+} from "react-icons/ai";
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return <div></div>;
 }
+HiOutlineUsers;
 
 export default Sidebar;
