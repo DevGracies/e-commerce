0796c75eff7a5ed68813aa0e02c536b1985da1e1
Grace <graceadegunle16@gmail.com>
2023-09-02 22:42:27 +0100

    commit

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <div className="flex w-32 h-5 m-16">
+        <Link to="/">Home</Link>
+        <h2>/ Contact</h2>
+      </div>
+      <div className="flex justify-center m-16">
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/component/Navigation/Cart.jsx
+++ src/component/Navigation/Cart.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import Counter from "./Counter";
+import Cart from "../Screens/Cart/Cart";
 
 const Carts = () => {
+  // const [first, setfirst] = useState(false);
+
   return (
     <div>
       <div className="relative">
-        <AiOutlineShoppingCart className="text-4xl m-3 text-2xl text-black font-medium hover:cursor-pointer" />
+        <AiOutlineShoppingCart
+          // onClick={() => setfirst(!first)}
+          className="text-4xl m-3  text-black font-medium hover:cursor-pointer"
+        />
         <div className=" absolute text-center -top-1 right-0 w-5 h-5 rounded-full bg-red-700 justify-center items-centernflex flex-row text-white text-xs ">
           <Counter />
         </div>
       </div>
+      {/* {first && <Cart />} */}
     </div>
   );
 };
